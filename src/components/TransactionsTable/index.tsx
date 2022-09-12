e5dc5e53c90cd126065debf2a5a1685807f604c3
Washington <washingtonjaaj@gmail.com>
2022-09-12 13:04:12 -0300

    alteração no header

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,18 +1,15 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+import { useContext} from "react";
+import { TransactionsContext } from "../../TransactionsContext";
 import { Container } from "./styles";
 
-
-
 export function TransationsTable() {
-
-
+const { transactions } = useContext(TransactionsContext);
 return (
 <Container>
     <table>
     <thead>
         <tr>
-        <th>T+itulo</th>
+        <th>Título</th>
         <th>Valor</th>
         <th>Categoria</th>
         <th>Data</th>
@@ -22,17 +19,19 @@ return (
         {transactions.map((transaction) => {
         return (
             <tr key={transaction.id}>
-                <td>{transaction.title}</td>
-                <td className={transaction.type}>
-                    {new Intl.NumberFormat("pt-BR", {
-                    style: "currency",
-                    currency: "BRL",
-                    }).format(transaction.amount)}
-                </td>
-                <td>{transaction.category}</td>
-                <td>
-                    {transaction.createdAt}
-                </td>
+            <td>{transaction.title}</td>
+            <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+                }).format(transaction.amount)}
+            </td>
+            <td>{transaction.category}</td>
+            <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                new Date(transaction.createdAt)
+                )}
+            </td>
             </tr>
         );
         })}
